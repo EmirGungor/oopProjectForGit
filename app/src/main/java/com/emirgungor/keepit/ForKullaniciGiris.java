6a447abd9bca9e751293bd9a418b795571ca544a
EmirGungor <emirgungor07@hotmail.com>
2022-12-25 02:09:54 +0300

    Oop Project

---
+++ app/src/main/java/com/emirgungor/keepit/ForKullaniciGiris.java
@@ -0,0 +1,9 @@
+package com.emirgungor.keepit;
+
+import android.view.View;
+
+public interface ForKullaniciGiris {
+    void getData();
+    void kullanicilariListele();
+    void kullaniciGirisi(View view);
+}
---
+++ app/src/main/java/com/emirgungor/keepit/ForMain.java
@@ -0,0 +1,10 @@
+package com.emirgungor.keepit;
+
+import android.view.View;
+
+public interface ForMain {
+    void getData();
+    void gruplariListele();
+    void addGroup(View view);
+    void deleteGroup(View view);
+}
